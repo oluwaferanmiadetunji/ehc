e6d0fd91157281fa8077d0b4e305c9903a7f6e6a
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-09-12 20:51:34 +0100

    .

--- src/config/app.js
+++ src/config/app.js
@@ -11,8 +11,8 @@ const hospital = require('../routes/hospital');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({extended: true, limit: '100mb', parameterLimit: 100000}));
 app.use(helmet());
 
 let morganFunction = function (tokens, req, res) {
