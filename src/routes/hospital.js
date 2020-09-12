ac654296a9369deaa2096481dd077d694257aa73
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-09-12 08:19:58 +0100

    .

--- src/routes/hospital.js
+++ src/routes/hospital.js
@@ -4,7 +4,7 @@ const getHospitals = require('../controllers/getHospitals');
 const createHospital = require('../controllers/createHospital');
 const auth = require('../middlewares/auth');
 
-router.get('/hospitals', auth, getHospitals);
+router.post('/hospitals', getHospitals);
 router.post('/hospitals/add', createHospital);
 
 module.exports = router;
--- src/routes/nurse.js
+++ src/routes/nurse.js
@@ -5,7 +5,7 @@ const createNurse = require('../controllers/createNurse');
 
 const auth = require('../middlewares/auth');
 
-router.post('/nurses', auth, getNurses);
+router.post('/nurses', getNurses);
 router.post('/nurses/add', createNurse);
 
 module.exports = router;
