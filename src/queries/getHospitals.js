46d879c6779431845f8e0197e30ab8d7cf1196d9
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-09-17 11:52:14 +0100

    .

--- src/queries/getHospitals.js
+++ src/queries/getHospitals.js
@@ -2,7 +2,7 @@ const Hospitals = require('../models/hospital');
 
 module.exports = async (location) => {
 	try {
-		const hospitals = await Hospitals.find({location});
+		const hospitals = await Hospitals.find({location: location});
 		return hospitals;
 	} catch (err) {
 		console.log(err);
