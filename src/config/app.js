0c79266d634785ce98993bdfae9310127f1722f7
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-10-23 23:03:33 +0100

    .

--- src/config/app.js
+++ src/config/app.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const user = require('../routes/user');
-const home = require('../routes/home');
 const notification = require('../routes/notification');
 const feedback = require('../routes/feedback');
 const nurse = require('../routes/nurse');
@@ -11,8 +10,8 @@ const hospital = require('../routes/hospital');
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({extended: true, limit: '100mb', parameterLimit: 100000}));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb', parameterLimit: 100000 }));
 app.use(helmet());
 
 let morganFunction = function (tokens, req, res) {
@@ -28,7 +27,6 @@ let morganFunction = function (tokens, req, res) {
 };
 app.use(morgan(morganFunction));
 
-app.use('/', home);
 app.use('/', user);
 app.use('/', notification);
 app.use('/', feedback);
